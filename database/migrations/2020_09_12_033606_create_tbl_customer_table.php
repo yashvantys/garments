80ed479875b8d422a72cad573c4bdb33d3e4a6e4
Yashvant Yadav <yashvant.yadav@kitelytech.com>
2020-09-12 09:14:26 +0530

    db migration added

---
+++ database/migrations/2020_09_12_033606_create_tbl_customer_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTblCustomerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbl_customer', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->enum('status', ['pending', 'approved', 'decline'])->default('approved');           
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tbl_customer');
+    }
+}
---
+++ database/migrations/2020_09_12_034115_create_tbl_product_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTblProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbl_product', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_name');
+            $table->float('price',8,2);            
+            $table->enum('status', ['approve', 'pending', 'decline'])->default('approve'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tbl_product');
+    }
+}
